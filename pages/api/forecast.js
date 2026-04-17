734eb2dfd0f37298ba444b680099e2a1fab4bd96
omarabdelnaby-ai <omar.abdelnaby@tabby.ai>
2026-04-17 08:45:04 +0200

    Delete pages directory

--- pages/api/forecast.js
+++
@@ -1,9 +0,0 @@
-import { fetchFromScript } from "../../lib/api";
-export default async function handler(req, res) {
-  try {
-    const data = await fetchFromScript("forecast");
-    res.status(200).json(data);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-}
